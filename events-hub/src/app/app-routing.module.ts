09120a2028acf01e00a9e7826fe83ea2af223acd
paula <paula.augustin@indecosoft.ro>
2022-07-18 20:57:31 +0300

    routes and toolbar mockup

--- events-hub/src/app/app-routing.module.ts
+++ events-hub/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventsListComponent } from './events-list/events-list.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'events-list', component: EventsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
